e599b7311540b0b110fbeabe2d556beabc100443
TriHoangZXC <hoanghuutri97@gmail.com>
2022-04-27 10:49:03 +0700

    add angular-todo-app using anggular-form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-todo-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-todo-app');
+    expect(app.title).toEqual('angular-todo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-todo-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-todo app is running!');
   });
 });
